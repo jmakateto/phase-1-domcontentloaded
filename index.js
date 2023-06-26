241b5f9841d6c42684c7f2d62e83651f3ffbc6c5
John mk UBUNTU <jmakateto@gmail.com>
2023-06-26 19:16:50 +0300

    completed assignement

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    // Code to be executed when the DOM has loaded
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    const paragraph = document.getElementById("text");
+    paragraph.textContent = "This is really cool!";
+  });
+  
